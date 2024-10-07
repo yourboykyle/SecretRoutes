1acfd78b89da03f01ab4bd1960621e588f07dd99
MrFast <mrfastkrunker@gmail.com>
2024-10-07 19:16:37 -0400

    Make AutoUpdateLib use new SSL Certificate

--- src/main/java/xyz/yourboykyle/secretroutes/utils/SSLUtils.java
+++ src/main/java/xyz/yourboykyle/secretroutes/utils/SSLUtils.java
@@ -3,7 +3,7 @@ package xyz.yourboykyle.secretroutes.utils;
 import javax.net.ssl.*;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
-
+import moe.nea.libautoupdate.UpdateUtils;
 
 public class SSLUtils {
 
@@ -16,7 +16,7 @@ public class SSLUtils {
                 LogUtils.error(e);
             }
             if(keyStore != null){
-                keyStore.load(SSLUtils.class.getResourceAsStream("/mykeystore.jks"), "changeit".toCharArray());
+                keyStore.load(SSLUtils.class.getResourceAsStream("/srmkeystore.jks"), "changeit".toCharArray());
             }else{
                 ChatUtils.sendChatMessage("[§3SRM§f] §cSomething went wrong wth ssl. Send the log file in the §1#support§f channel in the discord with a screenshot of this message.");
             }
@@ -30,16 +30,14 @@ public class SSLUtils {
 
             HttpsURLConnection.setDefaultSSLSocketFactory(ctx.getSocketFactory());
 
+            // Update SSL Connection on AutoUpdateLib
+            UpdateUtils.patchConnection(connection -> {
+                if(connection instanceof HttpsURLConnection) ((HttpsURLConnection) connection).setSSLSocketFactory(ctx.getSocketFactory());
+            });
         }catch(Exception e){
             ChatUtils.sendChatMessage("[§3SRM§f] §cSomething went wrong wth ssl. Send the log file in the §1#support§f channel in the discord with a screenshot of this message.");
             LogUtils.error(e);
         }
 
     }
-
-
-
-
-
-
 }
