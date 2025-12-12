9586393c031e6f8f200b870086990266b0ae3a4e
Kyle Lyon <kyle.lyon2542@gmail.com>
2025-12-11 19:58:58 -0500

    Bruh why are the polyfrost classes not being imported

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress("UnstableApiUsage", "PropertyName")
 
 import dev.deftu.gradle.utils.GameSide
+import groovy.lang.MissingPropertyException
 
 plugins {
     java
@@ -9,18 +10,17 @@ plugins {
     id("dev.deftu.gradle.tools") // Applies several configurations to things such as the Java version, project name/version, etc.
     id("dev.deftu.gradle.tools.resources") // Applies resource processing so that we can replace tokens, such as our mod name/version, in our resources.
     id("dev.deftu.gradle.tools.bloom") // Applies the Bloom plugin, which allows us to replace tokens in our source files, such as being able to use `@MOD_VERSION` in our source files.
-    id("dev.deftu.gradle.tools.shadow") // Applies the Shadow plugin, which allows us to shade our dependencies into our mod JAR. This is NOT recommended for Fabric mods, but we have an *additional* configuration for those!
     id("dev.deftu.gradle.tools.minecraft.loom") // Applies the Loom plugin, which automagically configures Essential's Architectury Loom plugin for you.
+    id("dev.deftu.gradle.tools.shadow") // Applies the Shadow plugin, which allows us to shade our dependencies into our mod JAR. This is NOT recommended for Fabric mods, but we have an *additional* configuration for those!
     id("dev.deftu.gradle.tools.minecraft.releases") // Applies the Minecraft auto-releasing plugin, which allows you to automatically release your mod to CurseForge and Modrinth.
 }
 
-val mod_name: String by project
-val mod_version: String by project
-val mod_id: String by project
-val mod_archives_name: String by project
+if (mcData.isForge) {
+    loom.forge.mixinConfig("mixins.${modData.id}.init.json")
+}
 
-version = mod_version
-group = "xyz.yourboykyle"
+val mod_archives_name: String = extra["mod.archives_name"]?.toString()
+    ?: throw MissingPropertyException("mod.archives_name has not been set.")
 
 base {
     archivesName.set(mod_archives_name)
@@ -28,11 +28,11 @@ base {
 
 toolkitLoomHelper {
     useOneConfig {
-        version = "1.0.0-alpha.106"
-        loaderVersion = "1.1.0-alpha.46"
+        version = "1.0.0-alpha.180"
+        loaderVersion = "1.1.0-alpha.53"
 
         usePolyMixin = true
-        polyMixinVersion = "0.8.4+build.2"
+        polyMixinVersion = "0.8.4+build.7"
 
         applyLoaderTweaker = true
 
@@ -41,59 +41,51 @@ toolkitLoomHelper {
         }
     }
 
-    useDevAuth("1.2.1")
-    useMixinExtras("0.4.1")
+    useDevAuth("1.2.+")
+    useMixinExtras("0.5.0")
+
+    useProperty("mixin.debug.export", "true", GameSide.BOTH)
 
     // Turns off the server-side run configs, as we're building a client-sided mod.
     disableRunConfigs(GameSide.SERVER)
 
     // Defines the name of the Mixin refmap, which is used to map the Mixin classes to the obfuscated Minecraft classes.
     if (!mcData.isNeoForge) {
-        useMixinRefMap(mod_id)
+        useMixinRefMap(modData.id)
     }
 
     if (mcData.isForge) {
         // Configures the Mixin tweaker if we are building for Forge.
-        useForgeMixin(mod_id)
-    }
-}
-
-sourceSets {
-    main {
-        java {
-            srcDir(rootProject.file("src/main/java/common"))
-            if (mcData.isLegacyForge) {
-                srcDir(rootProject.file("src/main/java/forge-1.8.9"))
-            } else if (mcData.isFabric) {
-                srcDir(rootProject.file("src/main/java/fabric-1.21.8"))
-            }
-        }
-        resources {
-            srcDir(rootProject.file("src/main/resources"))
-        }
-        output.setResourcesDir(java.classesDirectory)
+        useForgeMixin(modData.id)
     }
 }
 
 repositories {
+    maven("https://api.modrinth.com/maven") {
+        content { includeGroup("maven.modrinth") }
+    }
+    maven("https://maven.bawnorton.com/releases") {
+        content { includeGroup("com.github.bawnorton.mixinsquared") }
+    }
     maven("https://repo.nea.moe/releases")
 }
 
 dependencies {
-    // Add (Legacy) Fabric API as dependencies (these are both optional but are particularly useful).
-    if (mcData.isFabric) {
-        if (mcData.isLegacyFabric) {
-            // 1.8.9 - 1.13
-            modImplementation("net.legacyfabric.legacy-fabric-api:legacy-fabric-api:${mcData.dependencies.legacyFabric.legacyFabricApiVersion}")
-        } else {
-            // 1.16.5+
-            modImplementation("net.fabricmc.fabric-api:fabric-api:${mcData.dependencies.fabric.fabricApiVersion}")
-        }
-    }
-
-    // LibAutoUpdate - will be shaded into the jar
-    // Note: The Deftu shadow plugin automatically shades implementation dependencies
     implementation("moe.nea:libautoupdate:1.3.1") {
         isTransitive = false
+    }
+}
+
+tasks {
+    jar {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
+
+    remapJar {
+        manifest {
+            attributes(mapOf(
+                "MixinConfigs" to "mixins.${modData.id}.init.json,mixins.${modData.id}.json",
+            ))
+        }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,21 +21,51 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
+
     plugins {
-        kotlin("jvm") version("2.0.10")
-        id("dev.deftu.gradle.multiversion-root") version("2.64.0")
+        kotlin("jvm") version("2.2.10")
+        id("dev.deftu.gradle.multiversion-root") version("2.58.0")
     }
 }
 
-val mod_name: String = extra["mod_name"]?.toString()
-?: throw MissingPropertyException("mod_name has not been set.")
+val projectName: String = extra["mod.name"]?.toString()
+    ?: throw MissingPropertyException("mod.name has not been set.")
 
-rootProject.name = mod_name
+// Configures the root project Gradle name based on the value in `gradle.properties`
+rootProject.name = projectName
 rootProject.buildFileName = "root.gradle.kts"
 
+// Adds all of our build target versions to the classpath if we need to add version-specific code.
+// Update this list if you want to remove/add a version and/or mod loader.
+// The format is: version-modloader (f.ex: 1.8.9-forge, 1.17.1-fabric, etc)
+// **REMEMBER TO ALSO UPDATE THE `root.gradle.kts` AND `build.gradle.kts` FILES WITH THE NEW VERSION(S).
 listOf(
     "1.8.9-forge",
-    "1.21.8-fabric"
+    "1.8.9-fabric",
+
+    "1.12.2-forge",
+    "1.12.2-fabric",
+
+    "1.16.5-forge",
+    "1.16.5-fabric",
+
+    "1.20.1-forge",
+    "1.20.1-fabric",
+
+    "1.20.4-forge",
+    "1.20.4-fabric",
+
+    "1.21.1-neoforge",
+    "1.21.1-fabric",
+
+    "1.21.4-neoforge",
+    "1.21.4-fabric",
+
+    "1.21.5-neoforge",
+    "1.21.5-fabric",
+
+    "1.21.8-neoforge",
+    "1.21.8-fabric",
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
@@ -43,4 +73,3 @@ listOf(
         buildFileName = "../../build.gradle.kts"
     }
 }
-
