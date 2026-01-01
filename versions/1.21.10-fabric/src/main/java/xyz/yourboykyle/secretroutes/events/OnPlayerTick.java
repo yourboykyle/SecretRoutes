776b9511469f843871f54e7bab657b0dfecf74bb
Kyle Lyon <kyle.lyon2542@gmail.com>
2026-01-01 11:25:43 -0500

    Should fix the major game crash that randomly gets triggered

--- versions/1.21.10-fabric/src/main/java/xyz/yourboykyle/secretroutes/events/OnPlayerTick.java
+++ versions/1.21.10-fabric/src/main/java/xyz/yourboykyle/secretroutes/events/OnPlayerTick.java
@@ -39,76 +39,88 @@ public class OnPlayerTick {
     }
 
     private static void onPlayerTick(MinecraftClient client) {
-        ClientPlayerEntity player = client.player;
-        if (player == null) {
-            return;
-        }
+        try {
+            ClientPlayerEntity player = client.player;
+            if (player == null) {
+                return;
+            }
 
-        //If all secrets in the room have been completed
+            //If all secrets in the room have been completed
         /*if(Waypoints.allFound) {
             Main.currentRoom = new Room(null);
         }*/
 
-        // Draw Lines
-        if(SRMConfig.modEnabled) {
-            Main.currentRoom.renderLines();
-        }
-
-        if(Main.currentRoom.getSecretType() == Room.SECRET_TYPES.BAT) {
-            BlockPos pos = player.getBlockPos();
-            BlockPos batPos = Main.currentRoom.getSecretLocation();
-
-            if (pos.getX() >= batPos.getX() - 3 && pos.getX() <= batPos.getX() + 3 && pos.getY() >= batPos.getY() - 3 && pos.getY() <= batPos.getY() + 3 && pos.getZ() >= batPos.getZ() - 3 && pos.getZ() <= batPos.getZ() + 3) {
-                Main.currentRoom.nextSecret();
-                LogUtils.info("Went by bat at " + batPos);
+            // Draw Lines
+            if (SRMConfig.modEnabled) {
+                Main.currentRoom.renderLines();
             }
-        }
+
+            if (Main.currentRoom.getSecretType() == Room.SECRET_TYPES.BAT) {
+                BlockPos batPos = Main.currentRoom.getSecretLocation();
+
+                if (batPos != null) {
+                    BlockPos pos = player.getBlockPos();
+
+                    if (pos.getX() >= batPos.getX() - 3 && pos.getX() <= batPos.getX() + 3 && pos.getY() >= batPos.getY() - 3 && pos.getY() <= batPos.getY() + 3 && pos.getZ() >= batPos.getZ() - 3 && pos.getZ() <= batPos.getZ() + 3) {
+                        Main.currentRoom.nextSecret();
+                        LogUtils.info("Went by bat at " + batPos);
+                    }
+                }
+            }
 
         /* This has been commented out because it is causing it to log the secret multiple times if there are 2 secrets in a row in the route.
         This was originally added because in a specific room, you cannot get the item secret if it spawns and the velocity pushes the item away from you.
         But you can still get the secret if you walk over to the item secret, or just press the next secret keybind if you're lazy.
         */
-        if(Main.currentRoom.getSecretType() == Room.SECRET_TYPES.ITEM) {
-            BlockPos pos = player.getBlockPos();
-            BlockPos itemPos = Main.currentRoom.getSecretLocation();
+            if (Main.currentRoom.getSecretType() == Room.SECRET_TYPES.ITEM) {
+                BlockPos itemPos = Main.currentRoom.getSecretLocation();
 
-            if (pos.getX() >= itemPos.getX() - 2 && pos.getX() <= itemPos.getX() + 2 && pos.getY() >= itemPos.getY() - 2 && pos.getY() <= itemPos.getY() + 2 && pos.getZ() >= itemPos.getZ() - 2 && pos.getZ() <= itemPos.getZ() + 2) {
-                new Thread(() -> {
-                    try {
+                if (itemPos != null) {
+                    BlockPos pos = player.getBlockPos();
 
-                        Thread.sleep(1500);
-                        if (Main.currentRoom.getSecretType() == Room.SECRET_TYPES.ITEM && itemPos.getX() == Main.currentRoom.getSecretLocation().getX() && itemPos.getY() == Main.currentRoom.getSecretLocation().getY() && itemPos.getZ() == Main.currentRoom.getSecretLocation().getZ()) {
+                    if (pos.getX() >= itemPos.getX() - 2 && pos.getX() <= itemPos.getX() + 2 && pos.getY() >= itemPos.getY() - 2 && pos.getY() <= itemPos.getY() + 2 && pos.getZ() >= itemPos.getZ() - 2 && pos.getZ() <= itemPos.getZ() + 2) {
+                        new Thread(() -> {
+                            try {
 
-                            Main.currentRoom.nextSecret();
-                            LogUtils.info("Picked up item at " + itemPos + " (Auto)");
-                        }
-                    } catch (InterruptedException e1) {
-                        LogUtils.error(e1);
+                                Thread.sleep(1500);
+                                BlockPos currentItemPos = Main.currentRoom.getSecretLocation();
+                                if (Main.currentRoom.getSecretType() == Room.SECRET_TYPES.ITEM && currentItemPos != null && itemPos.getX() == currentItemPos.getX() && itemPos.getY() == currentItemPos.getY() && itemPos.getZ() == currentItemPos.getZ()) {
+
+                                    Main.currentRoom.nextSecret();
+                                    LogUtils.info("Picked up item at " + itemPos + " (Auto)");
+                                }
+                            } catch (InterruptedException e1) {
+                                LogUtils.error(e1);
+                            }
+                        }).start();
                     }
-                }).start();
-            }
-        }
-
-
-        // Route Recording
-        if(Main.routeRecording.recording) {
-            if (Main.routeRecording.previousLocation == null) {
-                BlockPos targetPos = new BlockPos((int)Math.floor(player.getX()), (int)Math.floor(player.getY()), (int)Math.floor(player.getZ()));
-                Main.routeRecording.addWaypoint(Room.WAYPOINT_TYPES.LOCATIONS, targetPos); // Trust the process
-                Main.routeRecording.previousLocation = player.getBlockPos();
-            } else {
-                BlockPos pos = player.getBlockPos();
-                BlockPos prevPos = Main.routeRecording.previousLocation;
-
-                double distance = Math.abs(Math.sqrt(Math.pow(pos.getX() - prevPos.getX(), 2) + Math.pow(pos.getY() - prevPos.getY(), 2) + Math.pow(pos.getZ() - prevPos.getZ(), 2)));
-
-                // If the player has moved 5 blocks or more from the previous waypoint
-                if (distance >= 2.4) {
-                    BlockPos targetPos = new BlockPos((int)Math.floor(player.getX()), (int)Math.floor(player.getY()), (int)Math.floor(player.getZ()));
-                    Main.routeRecording.addWaypoint(Room.WAYPOINT_TYPES.LOCATIONS, targetPos);
-                    Main.routeRecording.previousLocation = player.getBlockPos();
                 }
             }
+
+
+            // Route Recording
+            if (Main.routeRecording.recording) {
+                if (Main.routeRecording.previousLocation == null) {
+                    BlockPos targetPos = new BlockPos((int) Math.floor(player.getX()), (int) Math.floor(player.getY()), (int) Math.floor(player.getZ()));
+                    Main.routeRecording.addWaypoint(Room.WAYPOINT_TYPES.LOCATIONS, targetPos); // Trust the process
+                    Main.routeRecording.previousLocation = player.getBlockPos();
+                } else {
+                    BlockPos pos = player.getBlockPos();
+                    BlockPos prevPos = Main.routeRecording.previousLocation;
+
+                    double distance = Math.abs(Math.sqrt(Math.pow(pos.getX() - prevPos.getX(), 2) + Math.pow(pos.getY() - prevPos.getY(), 2) + Math.pow(pos.getZ() - prevPos.getZ(), 2)));
+
+                    // If the player has moved 5 blocks or more from the previous waypoint
+                    if (distance >= 2.4) {
+                        BlockPos targetPos = new BlockPos((int) Math.floor(player.getX()), (int) Math.floor(player.getY()), (int) Math.floor(player.getZ()));
+                        Main.routeRecording.addWaypoint(Room.WAYPOINT_TYPES.LOCATIONS, targetPos);
+                        Main.routeRecording.previousLocation = player.getBlockPos();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LogUtils.error(e);
+            e.printStackTrace();
         }
     }
 }
