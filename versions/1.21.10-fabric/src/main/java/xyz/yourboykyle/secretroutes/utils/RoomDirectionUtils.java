3b92062e0992cd9e9d71531d170b9a59ec786c4b
Chrisitian Steenkamp <christiannsteenkamp@gmail.com>
2026-01-16 21:38:23 +0200

    Correct room anchor position.

--- versions/1.21.10-fabric/src/main/java/xyz/yourboykyle/secretroutes/utils/RoomDirectionUtils.java
+++ versions/1.21.10-fabric/src/main/java/xyz/yourboykyle/secretroutes/utils/RoomDirectionUtils.java
@@ -9,31 +9,19 @@ public class RoomDirectionUtils {
     public static String roomDirection() {
         if (DungeonScanner.currentRoom == null) return "UNKNOWN";
 
-        String direction = switch (DungeonScanner.currentRoom.rotation) {
+        return switch (DungeonScanner.currentRoom.rotation) {
             case SOUTH -> "S";
             case WEST -> "W";
             case NORTH -> "N";
             case EAST -> "E";
             default -> "S";
         };
-
-        return direction;
     }
 
     public static BlockPos getRoomAnchor() {
         if (DungeonScanner.currentRoom == null) return null;
-        BlockPos clay = DungeonScanner.currentRoom.clayPos;
 
-        String dir = roomDirection();
-        int offset = 15;
-
-        return switch (dir) {
-            case "S" -> new BlockPos(clay.getX() - offset, clay.getY(), clay.getZ() - offset); // Default
-            case "N" -> new BlockPos(clay.getX() + offset, clay.getY(), clay.getZ() + offset); // Rotated 180
-            case "W" -> new BlockPos(clay.getX() + offset, clay.getY(), clay.getZ() - offset); // Rotated 90
-            case "E" -> new BlockPos(clay.getX() - offset, clay.getY(), clay.getZ() + offset); // Rotated 270
-            default -> clay;
-        };
+        return DungeonScanner.currentRoom.clayPos;
     }
 
     public static Point roomCorner() {
